efbaac5871871965fd7f443f84cc564c7fe6a3bc
Prathiba <87192181+prathibapradeep@users.noreply.github.com>
2025-06-17 22:04:30 +0100

    updateV4

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,4 +1,8 @@
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../pipeline')))
+
 from validate import parse_customer, parse_transaction  # type: ignore
 
 def test_parse_customer():
